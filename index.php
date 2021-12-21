37aed0502771325c4fb3b220f112e8b4d757c377
gsh519 <gsh05196yu@yahoo.co.jp>
2021-12-21 19:33:49 +0900

    db変更

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 // define('DB_USER', 'root');
 // define('DB_PASS', 'root');
 // define('DB_NAME', 'board');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$db_name = substr($url['path'], 1);
+$db_host = $url["host"];
+$user = $url["user"];
+$password = $url["pass"];
+$dsn = "mysql:dbname=" . $db_name . ";host=" . $db_host;
 
 //タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
@@ -19,7 +25,7 @@ try {
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
   ];
-  $pdo = new PDO('mysql:charset=UTF8;dbname=' . DB_NAME . ';host=' . DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, $option);
+  $pdo = new PDO($dsn, $user, $password, $option);
 } catch (PDOException $e) {
   //接続エラーの時のエラー内容を取得
   $error_message[] = $e->getMessage();
