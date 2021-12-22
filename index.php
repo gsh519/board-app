9f5c6e548df28a4f056eb897544b5074e543a37a
gsh519 <gsh05196yu@yahoo.co.jp>
2021-12-22 16:32:01 +0900

    pdo修正

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-require "./dbconnect.php";
-
-// $dbName = $_SERVER['DB_NAME'];
-// $host = $_SERVER['DB_HOST'];
-// $user = $_SERVER['DB_USER'];
-// $pass = $_SERVER['DB_PASS'];
+$dbName = $_SERVER['DB_NAME'];
+$host = $_SERVER['DB_HOST'];
+$user = $_SERVER['DB_USER'];
+$pass = $_SERVER['DB_PASS'];
 
 //タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
@@ -14,7 +12,17 @@ $error_message = [];
 
 session_start();
 
-dbConnect($_SERVER['DB_NAME'], $_SERVER['DB_HOST'], $_SERVER['DB_USER'], $_SERVER['DB_PASS']);
+//データベースに接続
+try {
+  $option = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+  ];
+  $pdo = new PDO('mysql:charset=UTF8;dbname=' . $dbName . ';host=' . $host, $user, $pass, $option);
+} catch (PDOException $e) {
+  //接続エラーの時のエラー内容を取得
+  $error_message[] = $e->getMessage();
+}
 
 if (!empty($_POST['btn_submit'])) {
   //投稿バリデーション
